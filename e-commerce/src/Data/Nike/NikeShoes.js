5e9cbf9c2f83a7b1cb086bc47aa5cac1faa4f056
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-27 16:34:39 +0530

    Nike shoes data

---
+++ e-commerce/src/Data/Nike/NikeShoes.js
@@ -0,0 +1,190 @@
+[
+  {
+    imageUrl: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/u_126ab356-44d8-4a06-89b4-fcdcc8df0245/d1bfe34b-0913-46fc-9a0f-6242736c02e6/air-jordan-4-rm.png",
+    brand: "Nike",
+    title: "Air Jordan 4 RM",
+    color: "Black",
+    discountedPrice: 5499,
+    price: 7999,
+    discountedPercent: 31,
+    sizes: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 45,
+      },
+    ],
+  },
+  {
+    imageUrl: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/1767ea8d-6819-471f-b710-207c1d0a600a/AIR+JORDAN+4+RM.png",
+    brand: "Nike",
+    title: "Nike Air Max 90",
+    color: "White/Black",
+    discountedPrice: 6999,
+    price: 9999,
+    discountedPercent: 30,
+    sizes: [
+      {
+        name: "7",
+        quantity: 15,
+      },
+      {
+        name: "8",
+        quantity: 25,
+      },
+      {
+        name: "9",
+        quantity: 35,
+      },
+      {
+        name: "10",
+        quantity: 20,
+      },
+    ],
+  },
+  {
+    imageUrl: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f61c9f7e-78d6-4d27-9503-f9a9f916071a/AIR+JORDAN+4+RM.png",
+    brand: "Nike",
+    title: "Nike Air Force 1 '07",
+    color: "White",
+    discountedPrice: 6299,
+    price: 8999,
+    discountedPercent: 30,
+    sizes: [
+      {
+        name: "6",
+        quantity: 10,
+      },
+      {
+        name: "7",
+        quantity: 20,
+      },
+      {
+        name: "8",
+        quantity: 30,
+      },
+      {
+        name: "9",
+        quantity: 25,
+      },
+      {
+        name: "10",
+        quantity: 15,
+      },
+    ],
+  },
+  {
+    imageUrl: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f8c56df2-6934-4fa8-b731-66f8ea14e5f2/AIR+JORDAN+4+RM.png",
+    brand: "Nike",
+    title: "Nike React Infinity Run Flyknit",
+    color: "Blue/Black",
+    discountedPrice: 7999,
+    price: 9999,
+    discountedPercent: 20,
+    sizes: [
+      {
+        name: "8",
+        quantity: 18,
+      },
+      {
+        name: "9",
+        quantity: 22,
+      },
+      {
+        name: "10",
+        quantity: 30,
+      },
+      {
+        name: "11",
+        quantity: 25,
+      },
+    ],
+  },
+  {
+    imageUrl: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/65c65015-13b8-4392-8e69-a8f5fffdf9c7/AIR+JORDAN+4+RM.png",
+    brand: "Nike",
+    title: "Nike Blazer Mid '77 Vintage",
+    color: "White/Black",
+    discountedPrice: 5799,
+    price: 8299,
+    discountedPercent: 30,
+    sizes: [
+      {
+        name: "6",
+        quantity: 12,
+      },
+      {
+        name: "7",
+        quantity: 18,
+      },
+      {
+        name: "8",
+        quantity: 25,
+      },
+      {
+        name: "9",
+        quantity: 20,
+      },
+      {
+        name: "10",
+        quantity: 15,
+      },
+    ],
+  },
+  {
+    imageUrl: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/d5b850ba-91ea-4652-8631-f4277ac3e67e/AIR+JORDAN+4+RM.png",
+    brand: "Nike",
+    title: "Nike Air Zoom Pegasus 37",
+    color: "Red/White",
+    discountedPrice: 7199,
+    price: 8999,
+    discountedPercent: 20,
+    sizes: [
+      {
+        name: "7",
+        quantity: 14,
+      },
+      {
+        name: "8",
+        quantity: 22,
+      },
+      {
+        name: "9",
+        quantity: 28,
+      },
+      {
+        name: "10",
+        quantity: 24,
+      },
+      {
+        name: "11",
+        quantity: 18,
+      },
+    ],
+  },
+  {
+    imageUrl:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/7580a51a-db7e-4a79-a8a7-e9296be06289/AIR+JORDAN+4+RM.png",
+    brand: "Nike",
+    title: "Nike Air VaporMax 2020 Flyknit",
+    color: "Grey/Blue",
+    discountedPrice: 9999,
+    price: 12499,
+    discountedPercent: 20,
+    sizes: [
+      {
+        name: "7",
+        quantity: 10,
+      },
+      {
+        name: "8",
+      },
+    ],
+  },
+];
